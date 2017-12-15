443889b1812fb11c598c472cdb1efb6aeef1f79c
Aaron Ramshaw <aaron.ramshaw@domain.com.au>
2017-12-15 14:04:36 +1100

    more prompt work

--- shell/prompt.zsh
+++ shell/prompt.zsh
@@ -9,10 +9,9 @@ function virtualenv_info {
 function git_prompt() {
   local ref="$(git rev-parse --symbolic-full-name --abbrev-ref HEAD 2>/dev/null)"
   if [ -n "$ref" ]; then
-    echo " on %{$fg_bold[magenta]%}${ref#refs/heads/}%{$reset_color%}"
+    echo "(%{$fg_bold[magenta]%}${ref#refs/heads/}%{$reset_color%}) "
   fi
 }
 
-PROMPT='%{$fg_bold[red]%}%n%{$reset_color%} at %{$fg_bold[green]%}${PWD/#$HOME/~}%{$reset_color%}$(git_prompt)
-$(virtualenv_info)%(?,,%{${fg_bold[white]}%}%{$reset_color%} )$ '
+PROMPT='$(git_prompt)%{$fg_bold[green]%}${PWD/#$HOME/~}%{$reset_color%} $ '
 RPROMPT=''
---
+++ shell/virtualenv.sh
@@ -0,0 +1,5 @@
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+  source /usr/local/bin/virtualenvwrapper.sh
+fi
+
+export VIRTUAL_ENV_DISABLE_PROMPT=
