e71fe995bc23f5f26c64ad9e194419eb81948637
Aaron Ramshaw <aaron.ramshaw@domain.com.au>
2017-12-18 16:41:33 +1100

    git autocomplete

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shel
 ln -s `pwd`/shell ~/.shell
 ln -s `pwd`/shell/zshrc ~/.zshrc
 ln -s `pwd`/shell/bashrc ~/.bashrc
+
+# Initialise for autocompletion
+rm -f ~/.zcompdump; compinit
