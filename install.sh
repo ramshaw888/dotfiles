843d8427e3481629abddf779708ae7b068e3dac1
Aaron Ramshaw <aaron.ramshaw@domain.com.au>
2018-01-19 11:10:46 +1100

    improve install

--- install.sh
+++ install.sh
@@ -1,12 +1,37 @@
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+function base {
+  if [ ! -d $HOME/.config ]; then
+    mkdir $HOME/.config
+  fi
+}
 
-ln -s `pwd`/shell ~/.shell
-ln -s `pwd`/shell/zshrc ~/.zshrc
-ln -s `pwd`/shell/bashrc ~/.bashrc
+function shell {
+  base
+  git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
-mkdir ~/.config
-mkdir ~/.config/nvim
-ln -s `pwd`/vimrc ~/.config/nvim/init.vim
+  ln -s `pwd`/shell ~/.shell
+  ln -s `pwd`/shell/zshrc ~/.zshrc
+  ln -s `pwd`/shell/bashrc ~/.bashrc
 
-# Initialise for autocompletion
-rm -f ~/.zcompdump; compinit
+  # Initialise for autocompletion
+  rm -f ~/.zcompdump; compinit
+}
+
+function vim {
+  base
+  if [ ! -d $HOME/.config/nvim ]; then
+    mkdir $HOME/.config/nvim
+  fi
+  ln -s `pwd`/vimrc $HOME/.config/nvim/init.vim
+}
+
+case $1 in
+  "shell")
+    shell
+    ;;
+  "vim")
+    vim
+    ;;
+  *)
+    echo "install vim, shell"
+    ;;
+esac
