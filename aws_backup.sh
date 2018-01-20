9c237da0982dc2b21cc124618fa4e604dc65668a
Aaron Ramshaw <aaronramshaw@gmail.com>
2018-01-21 00:20:19 +1100

    AWS backup

---
+++ aws_backup.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+DRY="--dryrun"
+
+function sync {
+  SOURCE=$1
+  TARGET=$2
+
+  if [ "$3" == "--live" ]
+  then
+    DRY=""
+  fi
+
+  echo "Sync $DRY"
+
+  aws s3 sync $SOURCE $TARGET $DRY \
+    --exclude="*.DS_Store" \
+    --exclude="*Backups" \
+    --exclude="Lightroom*" \
+    --exclude="*.db*"
+}
+
+function inc {
+  while read -r line;
+  do
+    ((count++));
+    echo -ne "\r$count";
+  done
+  echo "\nDone"
+}
+
+function check {
+  SOURCE=$1
+  TARGET=$2
+
+  echo "Checking sync"
+
+  count=0
+  aws s3 sync $SOURCE $TARGET $DRY \
+    --exclude="*.DS_Store" \
+    --exclude="*Backups" \
+    --exclude="Lightroom*" \
+    --exclude="*.db*" | inc
+}
+
+function clean {
+  TARGET=$1
+  DRY="--dryrun"
+
+  if [ "$2" == "--live" ]
+  then
+    DRY=""
+  fi
+
+  echo "Removing .DS_Store files"
+
+  aws s3 rm $TARGET $DRY \
+  --recursive \
+  --exclude="*" \
+  --include="*.DS_Store"
+}
+
+
+case $1 in
+  "sync")
+    sync $2 $3 $4
+    ;;
+  "check")
+    check $2 $3
+    ;;
+  "clean")
+    clean $2 $3
+    ;;
+  *)
+    echo "aws-backup: sync, check, clean"
+    ;;
+esac
