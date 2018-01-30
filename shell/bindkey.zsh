380c6b679422007d420b37ce8f8fb707045a851d
Aaron Ramshaw <aaron.ramshaw@domain.com.au>
2018-01-30 15:00:10 +1100

    Search history better

--- shell/bindkey.zsh
+++ shell/bindkey.zsh
@@ -1,10 +1,14 @@
 bindkey -v
+
 bindkey -M viins '^r' history-incremental-search-backward
 bindkey -M vicmd '^r' history-incremental-search-backward
+
 bindkey -M vicmd 'i' vi-forward-char
 bindkey -M vicmd 'h' vi-backward-char
-bindkey -M vicmd 'e' up-line-or-history
-bindkey -M vicmd 'n' down-line-or-history
+bindkey -M vicmd 'e' up-history
+bindkey -M vicmd 'n' down-history
+bindkey -M vicmd 'E' up-line-or-history
+bindkey -M vicmd 'N' down-line-or-history
 bindkey -M vicmd 's' vi-insert
 bindkey -M vicmd 'b' vi-backward-word
 bindkey -M vicmd 'k' vi-forward-word
