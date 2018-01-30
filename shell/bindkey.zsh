0e844e43a1a1efc1197ceb0d74d8a83be1ac59bb
Aaron Ramshaw <aaron.ramshaw@domain.com.au>
2018-01-30 14:09:38 +1100

    Add bindkey

---
+++ shell/bindkey.zsh
@@ -0,0 +1,10 @@
+bindkey -v
+bindkey -M viins '^r' history-incremental-search-backward
+bindkey -M vicmd '^r' history-incremental-search-backward
+bindkey -M vicmd 'i' vi-forward-char
+bindkey -M vicmd 'h' vi-backward-char
+bindkey -M vicmd 'e' up-line-or-history
+bindkey -M vicmd 'n' down-line-or-history
+bindkey -M vicmd 's' vi-insert
+bindkey -M vicmd 'b' vi-backward-word
+bindkey -M vicmd 'k' vi-forward-word
