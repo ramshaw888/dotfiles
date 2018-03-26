9ff992df817bcc6cea47e1d8991e6ae953be2d6a
Aaron Ramshaw <aaronramshaw@gmail.com>
2018-03-26 19:24:38 +1100

    Change function names in install.sh to prevent collisions with actual programs

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ function base {
   fi
 }
 
-function shell {
+function shell_install {
   base
   git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
 
@@ -24,7 +24,7 @@ function shell {
   zsh -c "source `pwd`/shell/zshrc; compinit"
 }
 
-function vim {
+function vim_install {
   base
   if [ ! -d $HOME/.config/nvim ]; then
     mkdir $HOME/.config/nvim
@@ -33,36 +33,37 @@ function vim {
   ln -s `pwd`/vim/vimrc $HOME/.config/nvim/init.vim
 }
 
-function less {
+function less_install {
   lesskey -o $HOME/.less lesskey
 }
 
-function tern {
+function tern_install {
   npm i -g tern-jsx
   rm $HOME/.tern-config
   ln -s `pwd`/vim/.tern-config $HOME/.tern-config
 }
 
-function gpg {
+function gpg_install {
+  gpg --list-secret-keys --keyid-format LONG
   echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
   gpgconf --kill gpg-agent
 }
 
 case $1 in
   "shell")
-    shell
+    shell_install
     ;;
   "vim")
-    vim
+    vim_install
     ;;
   "less")
-    less
+    less_install
     ;;
   "tern")
-    tern
+    tern_install
     ;;
   "gpg")
-    gpg
+    gpg_install
     ;;
   *)
     echo "install vim, shell, less, tern"
