c1cd76c251e404ed9636657abc7bdadd10d46dda
Aaron Ramshaw <aaron.ramshaw@spaceship.com.au>
2023-03-03 11:20:58 +0800

    add graphql server, add some python stuff to path

--- vim/init.lua
+++ vim/init.lua
@@ -169,7 +169,7 @@ end
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-for _, lsp in pairs({ 'gopls', 'tsserver', 'terraformls', 'tflint', 'yamlls', 'pyright', 'sumneko_lua' }) do
+for _, lsp in pairs({ 'gopls', 'tsserver', 'terraformls', 'tflint', 'yamlls', 'pyright', 'sumneko_lua', 'graphql' }) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
