115fda798e2152563bdda1cd0c76f05a929e5f90
Aaron Ramshaw <aaronramshaw@gmail.com>
2025-03-31 11:48:56 +1100

    findreplace

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -24,3 +24,26 @@ lsrepos() {
   repos=$( echo $repos | while read dir; do [ -d "$dir/.git" ] && echo ${dir#"$CODEDIR/"}; done )
   echo $repos"\n"$(extraRepos)
 }
+
+findreplace() {
+    if [ $# -ne 2 ]; then
+        echo "Usage: findreplace \"text_to_find\" \"text_to_replace\""
+        return 1
+    fi
+
+    # Check if ag is installed
+    if ! command -v ag >/dev/null 2>&1; then
+        echo "Error: ag (The Silver Searcher) is not installed."
+        echo "Install it with: brew install ag (on macOS with Homebrew)"
+        return 1
+    fi
+
+    local search_pattern="$1"
+    local replace_text="$2"
+    local current_dir="$(pwd)"
+
+    # First show which files will be affected (preview) using ag
+    echo "Files that will be modified:"
+    ag -l "$search_pattern" "$current_dir"
+    ag -l "$search_pattern" "$current_dir" | xargs perl -i -pe "s/$search_pattern/$replace_text/g"
+}
